6484f1a4daba14d1110edd87b50b51a5a685a5ac
nueapop <nueapop.mo@gmail.com>
2022-07-06 00:45:36 +0700

    update: Input Statement

--- Analog and Digital Sensor/Input Statement/main.ino
+++ Analog and Digital Sensor/Input Statement/main.ino
@@ -1,3 +1,24 @@
 /*
     รับข้อมูลทาง console หากมีค่าเป็น “A” ให้แสดงชื่อ หากเป็น “B” ให้แสดงรหัส
 */
+
+void setup()
+{
+    Serial.begin(115200);
+}
+
+void loop()
+{
+    if (Serial.available() > 0)
+    {
+        String input = Serial.readString();
+        if (input == "A")
+        {
+            Serial.println("Name");
+        }
+        else if (input == "B")
+        {
+            Serial.println("12345");
+        }
+    }
+}
