bd738cdb16674d4d65754811b7aaf5c2d1356351
nueapop <nueapop.mo@gmail.com>
2022-07-06 02:08:01 +0700

    update: Advance Statement

--- Analog and Digital Sensor/Advance Statement/main.ino
+++ Analog and Digital Sensor/Advance Statement/main.ino
@@ -5,3 +5,47 @@
     “+” ให้กระพริบเร็วขึ้น 100ms
     “-” ให้กระพริบช้าลง 100ms
 */
+
+int D = 1000;
+
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(BUILTIN_LED, OUTPUT);
+}
+
+void loop()
+{
+    if (Serial.available() > 0)
+    {
+        unsigned char c = Serial.read();
+        if (c == '1')
+        {
+            D = 1000;
+        }
+        else if (c == '0')
+        {
+            D = 0;
+        }
+        else if (c == '+')
+        {
+            D -= 100;
+        }
+        else if (c == '-')
+        {
+            D += 100;
+        }
+    }
+    else
+    {
+        Serial.print("Delay = ");
+        Serial.println(D);
+        if (D > 0)
+        {
+            digitalWrite(BUILTIN_LED, LOW);
+            delay(D);
+            digitalWrite(BUILTIN_LED, HIGH);
+            delay(D);
+        }
+    }
+}
