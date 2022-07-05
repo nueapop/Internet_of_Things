be54137e22dca8d6ddf08276acedcba43dc0e866
nueapop <nueapop.mo@gmail.com>
2022-07-06 00:47:29 +0700

    update: Hello IoT World

--- Analog and Digital Sensor/Hello IoT World/main.ino
+++ Analog and Digital Sensor/Hello IoT World/main.ino
@@ -1,3 +1,14 @@
 /*
     แสดงข้อความ “Hello IoT World” ทางจอ console ทุก ๆ วินาที
 */
+
+void setup()
+{
+    Serial.begin(115200);
+}
+
+void loop()
+{
+    Serial.println("Hello IoT World");
+    delay(1000);
+}
