63aa84a5be796b48ebf7ccd26b8b1737eae315c3
nueapop <nueapop.mo@gmail.com>
2022-07-06 00:35:16 +0700

    update: Input To Console

--- Analog and Digital Sensor/Input To Console/main.ino
+++ Analog and Digital Sensor/Input To Console/main.ino
@@ -1,3 +1,16 @@
 /*
     รับข้อมูลทาง console และแสดงข้อมูลที่รับมาทาง console
 */
+
+void setup()
+{
+    Serial.begin(115200);
+}
+
+void loop()
+{
+    if (Serial.available() > 0)
+    {
+        Serial.println("Input String -> " + Serial.readString());
+    }
+}
